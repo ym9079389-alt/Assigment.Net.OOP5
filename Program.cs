7ac9332737109a7f6bebdff0ebc456e26bb7043b
ym9079389-alt <ym9079389@gmail.com>
2026-09-24 07:00:28 +0300

    Q1 => a

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
-﻿namespace Assigment.Net.OOP5
+﻿using System.Drawing;
+using System.Net;
+
+namespace Assigment.Net.OOP5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            #region Q1
 
+            #region a
+            //a) What happens when you assign one object variable to another object variable?
+            //Only the reference is copied, not the object itself. Both variables point to the same location in memory.
+            #endregion
+
+            #endregion
         }
     }
 }
