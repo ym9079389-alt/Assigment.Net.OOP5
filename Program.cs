2fed756396a53abf534d2ac093d2b35ac633c99f
ym9079389-alt <ym9079389@gmail.com>
2026-09-24 07:18:32 +0300

    Q1 => c

--- Program.cs
+++ Program.cs
@@ -14,9 +14,15 @@ namespace Assigment.Net.OOP5
             //Only the reference is copied, not the object itself. Both variables point to the same location in memory.
             #endregion
 
-            #region
+            #region b
             //Does assigning one object to another create a new object? Explain.
             //No.It only copies the reference in the Heap, so Both variables point to the same location in memory.
+            #endregion
+
+            #region c
+            //c) What is the difference between copying an object and copying its reference ?
+            //Copying Reference: Creates a new variable pointing to the same object.
+            //Copying Object: Creates a new object in memory containing a copy of the original object.
             #endregion
 
             #endregion
